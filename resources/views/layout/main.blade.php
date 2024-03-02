b164ac090f6e5a7c9c4624d5cfbe8d5126bb1077
GabrielFogo <gabrielcostal164@gmail.com>
2024-03-02 15:59:59 -0300

    Trocando o projeto de bootstrap para tailwind

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -1,43 +1,115 @@
 <!DOCTYPE html>
 <html lang="{{str_replace('_','-', app()->getLocale())}}">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>@yield('title')</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
-            rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
-            crossorigin="anonymous">
-    </head>
-    <body>
-    <header>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="/">Agenda de Eventos</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>@yield('title')</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+<header>
+    <nav class="p-5 flex justify-between items-center bg-gray-700 text-white border-b border-gray-200">
+        <a class="text-3xl leading-none" href="/">
+            Agenda de eventos
+        </a>
+        <div class="md:hidden">
+            <button class="navbar-burger flex items-center text-gray-900 p-3">
+                <svg class="block h-4 w-4 fill-current" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                    <title>Mobile menu</title>
+                    <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
+                </svg>
+            </button>
+        </div>
+        <ul class="hidden md:flex">
+            <li><a class="text-gray-400 hover:text-blue-500 pe-3" href="/">Eventos</a></li>
+            <li><a class="text-gray-400 hover:text-blue-500 pe-3" href="/events/create">Criar Eventos</a></li>
+            <li><a class="text-gray-400 hover:text-blue-500 pe-3" href="#">Entrar</a></li>
+            <li><a class="text-gray-400 hover:text-blue-500 pe-3" href="#">Cadastrar</a></li>
+        </ul>
+    </nav>
+    <div class="navbar-menu relative z-50 hidden">
+        <div class="navbar-backdrop fixed inset-0 bg-gray-800 opacity-25"></div>
+        <nav class="fixed top-0 left-0 bottom-0 flex flex-col w-5/6 max-w-sm py-6 px-6 bg-white border-r overflow-y-auto">
+            <div class="flex items-center mb-8">
+                <a class="text-2xl leading-none" href="/">
+                    Agenda de eventos
+                </a>
+                <button class="navbar-close ms-2">
+                    <svg class="h-6 w-6 text-gray-400 cursor-pointer hover:text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
+                    </svg>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link" aria-current="page" href="/">Eventos</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/events/create">Criar Eventos</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Entrar</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Cadastrar</a>
-                        </li>
-                    </ul>
-                </div>
+            </div>
+            <div>
+                <ul>
+                    <li class="mb-1">
+                        <a class="block p-4 text-sm font-semibold text-gray-400 hover:bg-blue-50 hover:text-blue-600 rounded" href="#">Eventos</a>
+                    </li>
+                    <li class="mb-1">
+                        <a class="block p-4 text-sm font-semibold text-gray-400 hover:bg-blue-50 hover:text-blue-600 rounded" href="#">Criar Eventos</a>
+                    </li>
+                    <li class="mb-1">
+                        <a class="block p-4 text-sm font-semibold text-gray-400 hover:bg-blue-50 hover:text-blue-600 rounded" href="#">Entrar</a>
+                    </li>
+                    <li class="mb-1">
+                        <a class="block p-4 text-sm font-semibold text-gray-400 hover:bg-blue-50 hover:text-blue-600 rounded" href="#">Cadastrar</a>
+                    </li>
+                </ul>
             </div>
         </nav>
-    </header>
+</header>
+<main class="p-5">
     @yield('content')
-    <footer>
-        <p>Agenda de eventos &copy; 2024</p>
-    </footer>
-    </body>
+</main>
+<footer class="text-center bg-gray-700 text-white py-3 absolute bottom-0 w-full">
+    <div class="rounded-t-lg">
+        <p class="mb-0">Agenda de eventos &copy; 2024</p>
+    </div>
+</footer>
+
+</div>
+</body>
+
+<script>
+    // Burger menus
+    document.addEventListener('DOMContentLoaded', function() {
+        // open
+        const burger = document.querySelectorAll('.navbar-burger');
+        const menu = document.querySelectorAll('.navbar-menu');
+
+        if (burger.length && menu.length) {
+            for (var i = 0; i < burger.length; i++) {
+                burger[i].addEventListener('click', function() {
+                    for (var j = 0; j < menu.length; j++) {
+                        menu[j].classList.toggle('hidden');
+                    }
+                });
+            }
+        }
+
+        // close
+        const close = document.querySelectorAll('.navbar-close');
+        const backdrop = document.querySelectorAll('.navbar-backdrop');
+
+        if (close.length) {
+            for (var i = 0; i < close.length; i++) {
+                close[i].addEventListener('click', function() {
+                    for (var j = 0; j < menu.length; j++) {
+                        menu[j].classList.toggle('hidden');
+                    }
+                });
+            }
+        }
+
+        if (backdrop.length) {
+            for (var i = 0; i < backdrop.length; i++) {
+                backdrop[i].addEventListener('click', function() {
+                    for (var j = 0; j < menu.length; j++) {
+                        menu[j].classList.toggle('hidden');
+                    }
+                });
+            }
+        }
+    });
+</script>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,29 @@
 @extends('layout.main')
 @section('title','Agenda de Eventos')
 @section('content')
-    <h1>Algum Titulo</h1>
-    @if($nome == 'gabriel')
-        <p>{{$nome}}</p>
-    @endif
-    @for($i = 0; $i < count($arr); $i++)
-        {{$arr[$i]}}
-    @endfor
+    <h1 class="text-2xl mb-1">
+        Busque um Evento
+    </h1>
+    <input type="text" name="evento" class="w-[100%] border border-gray rounded-md p-1 outline-none focus:border-gray-700 mb-1">
+    <div class="mb-1">
+        <h1 class="text-2xl">
+            Proximos eventos
+        </h1>
+        <p class="text-gray-500">Veja os proximos eventos</p>
+    </div>
+    <div class="flex">
+        @foreach($events as $event)
+            <div class="w-[25%] mt-2 border border-gray rounded-md">
+                <img src="{{asset('img/imagem-evento.jpg')}}" class="rounded-t-md max-h-[150px] w-[100%]">
+                <div class="mt-2 pb-3 px-3">
+                    <p class="text-sm text-gray-400">12/12/12</p>
+                    <h5 class="text-xl mb-[30px]">{{$event->title}}</h5>
+                    <p class="text-gray-500">X participantes</p>
+                    <div class="my-5">
+                        <a class="px-5 py-2 bg-blue-500 rounded-md text-white hover:bg-blue-700 ">Saiba mais</a>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
 @endsection
